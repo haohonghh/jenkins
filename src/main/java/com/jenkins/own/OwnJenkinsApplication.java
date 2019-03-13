f6d727dc3a6eadb843c5a6fc3fbaca2e0741d4b5
own <haohong278636029@vip.qq.com>
2019-03-13 11:44:39 +0800

    test jenkins

---
+++ src/main/java/com/jenkins/own/OwnJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.own;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwnJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OwnJenkinsApplication.class, args);
+    }
+
+}
